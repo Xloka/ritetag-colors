bdd344b3f940919ad0728f57e429f9e3cc6308b6
Xloka <a.saleh.ismael@gmail.com>
2016-04-27 12:42:27 +0200

    fix allow origin headers

--- server examples/ritetagApiLaravel/app/Http/Controllers/api.php
+++ server examples/ritetagApiLaravel/app/Http/Controllers/api.php
@@ -16,15 +16,7 @@ class api extends Controller
             $consumer_secret    = config('ritetag.consumer_secret');
             $oauth_token        = config('ritetag.oauth_token');
             $oauth_token_secret = config('ritetag.oauth_token_secret');
-            $domain = config('ritetag.domain');
-            if(isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER'])) {
-              die("no referer");
-              $ref = $_SERVER['HTTP_REFERER'];
-              $refData = parse_url($ref);
-              if ($refData['host'] !== $domain) {
-                die("your domain is not permitted");    
-              }
-            }
+            $domain = config('ritetag.domain');            
             $client = new Client($consumer_key, $consumer_secret, $oauth_token, $oauth_token_secret);
 	        $hashtags = json_decode($_GET['hashtags']);
 	        $output = array();
@@ -33,6 +25,8 @@ class api extends Controller
 	            $output[] = $data['stats'];
 	        }
 
-	        echo json_encode($output);
+	       return response()
+            ->json($output)
+            ->header('Access-Control-Allow-Origin' , $domain);
     }
 }
--- server examples/ritetagApiLaravel/config/ritetag.php
+++ server examples/ritetagApiLaravel/config/ritetag.php
@@ -5,5 +5,5 @@ return [
     'consumer_secret' => '',
     'oauth_token' => '',
     'oauth_token_secret' => '',
-    'domain' => 'localhost'
+    'domain' => 'http://127.0.0.1:8080'
 ];
